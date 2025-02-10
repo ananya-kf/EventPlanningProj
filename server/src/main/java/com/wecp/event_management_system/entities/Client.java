2fe41a6982a0ffc4287ca670742cebc4321f72d8
username <wecp-dummy@gmail.com>
2025-02-10 12:30:06 +0000

    backend

--- server/src/main/java/com/wecp/event_management_system/entities/Client.java
+++ server/src/main/java/com/wecp/event_management_system/entities/Client.java
@@ -3,6 +3,7 @@ package com.wecp.event_management_system.entities;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+//new
 @Entity
 @Table(name = "clients")
 public class Client extends User{
